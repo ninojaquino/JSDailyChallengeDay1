4fd5c6fd8b181d83f99d1cceaf1201719694e6a9
ninojaquino <ninojaquino@gmail.com>
2022-08-08 21:05:36 -0700

    pushing take home challenge

---
+++ js/script.js
@@ -0,0 +1,9 @@
+var myName = "Nino Jesus Aquino";
+//declare my name
+var favFood= "Ribeye"
+
+
+console.log('My name is ' + myName);
+console.log('My favorite food is ' + favFood);
+
+/*console logs of declaring my name and my favorite food*/
